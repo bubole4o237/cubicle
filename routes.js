c1383b261d9263dcc20d38755febe1d1d9d31fe0
Iavor <iavor20lite@gmail.com>
2021-02-16 17:38:10 +0200

    Add controllers and page not found

--- routes.js
+++ routes.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const router = Router();
+const productController = require('./controllers/productController');
+const aboutController = require('./controllers/aboutController');
 
-router.get('/', (req,res) => {
-    res.render('home', {layout: false});
-});
 
-router.get('/about', (req, res) => {
-    res.render('about', {layout: false});
+router.use('/about', aboutController);
+router.use('/', productController);
+router.use('*', (req, res) => {
+    res.render('404', {layout: false});
 });
 
 module.exports = router;
