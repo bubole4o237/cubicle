53dc2fdc996d53f60b20181d60d52ed90c5d87b3
Iavor <iavor20lite@gmail.com>
2021-02-18 19:28:07 +0200

    Restore server settings

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const config = require('./config/config');
+const config = require('./config/config');
 const routes = require('./routes');
 const app = express();
 
@@ -11,4 +11,4 @@ require('./config/express')(app);
 
 app.use(routes);
   
-app.listen(5000, () => console.log(`The server is running on port 5000...`));
+app.listen(config.PORT, () => console.log(`The server is running on port ${config.PORT}...`));
